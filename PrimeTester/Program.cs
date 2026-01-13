1fe3022f6f12ca66acc0a679c0eff6cb31977c62
aldovilardy <aldovilardy@gmail.com>
2026-01-13 12:29:36 -0500

    Add PrimeTester and ReverseString console projects
    Added two new .NET 10.0 console apps: PrimeTester (recursive prime check) and ReverseString (in-place string reversal). Updated solution file to include both projects. Main programs demonstrate usage and output.

---
+++ PrimeTester/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Prime Tester
+
+A prime number is a whole number that has no other divisors other than
+itself and 1.
+
+Write a function that accepts a number and returns true if it's
+ a prime number, false if it's not.
+
+Example:
+const numberToTest = 17;
+console.log(primeTester(numberToTest)); 
+// true
+
+ */
+
+const int numberToTest = 17;
+Console.WriteLine(PrimeTester(numberToTest));
+
+/// <summary>
+/// Determines if a number is prime using recursion.
+/// </summary>
+/// <param name="number">The number to check for primality.</param>
+/// <param name="divisor">Current divisor (used internally).</param>
+/// <returns>True if the number is prime; otherwise, false.</returns>
+static bool PrimeTester(int number, int divisor = 2) =>
+    number > 1 && (divisor * divisor > number || (number % divisor != 0 && PrimeTester(number, divisor + 1)));
---
+++ ReverseString/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+Reverse a string
+
+Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+
+Note: You may assume all the characters consist of printable ascii characters.
+
+Example :
+Input: “qwerty”
+
+Output: “ytrewq”
+
+**
+ * @param {char[]} s
+ * @return {string}
+ *
+var reverseString = function(s) {
+};
+
+*/
+
+Console.WriteLine(new string([.. "qwerty".ToCharArray().Reverse()])); // Output: "ytrewq" 
+
+Console.WriteLine(ReverseString("qwerty".ToCharArray())); // Output: "ytrewq"
+
+/// <summary>
+/// Reverses the input character array in place and returns the reversed string.
+/// </summary>
+/// <param name="array">The character array to be reversed.</param>
+/// <returns>The reversed string.</returns>
+/// <exception cref="ArgumentNullException">Throws ArgumentNullException if the input array is null.</exception>
+/// <remarks>
+/// This method modifies the input array in place and does not allocate extra space for another array.
+/// Time complexity is O(n).
+/// Space complexity is O(1).
+/// </remarks>
+static string ReverseString(char[] array)
+{
+    ArgumentNullException.ThrowIfNull(array);
+
+    if (array.Length == 0)
+        return string.Empty;
+
+    int start = 0;
+    int end = array.Length - 1;
+
+    while (start < end)
+    {
+        (array[end], array[start]) = (array[start], array[end]);
+        start++;
+        end--;
+    }
+
+    return new string(array);
+}
